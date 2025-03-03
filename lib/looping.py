1fdbc728873fa5c30ce3dc92fd715d1cd6376f26
VictorIgati <victor.igati@student.moringaschool.com>
2025-03-03 16:23:16 +0300

    done loops

--- lib/looping.py
+++ lib/looping.py
@@ -1,13 +1,29 @@
 #!/usr/bin/env python3
 
 def happy_new_year():
-    # code goes here!
-    pass
+    i =10
+    while i > 0:
+        print(i)
+        i -= 1
+    print("Happy New Year!")
 
-def square_integers(int_list):
-    # code goes here!
-    pass
+
+def square_integers(list_int):
+    square_int = []
+
+    for i in list_int:
+        square_int.append(i**2)
+
+    return square_int    
+
 
 def fizzbuzz():
-    # code goes here!
-    pass
+    for i in range(1, 101):
+        if i % 3 == 0 and i % 5 == 0:
+            print("FizzBuzz")
+        elif i % 3 == 0:
+            print("Fizz")
+        elif i % 5 == 0:
+            print("Buzz")
+        else:
+            print(i)
